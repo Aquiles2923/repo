acff4868d69ceff3a21c973164d9aea0ca771b3b
ojeto <137434439+ojeto@users.noreply.github.com>
2023-07-13 16:07:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -68,10 +68,10 @@ def update_channel_dict(message_content, channel_dict):
                 channel_name = channel_name.replace("BarÃ§a", "Barça")
             elif "beIN SPORTS Ã±" in channel_name:
                 channel_name = channel_name.replace("beIN SPORTS Ã±", "beIN SPORTS ñ")
-            elif "M.L. Campeones" in channel_name:
-                channel_name = channel_name.replace("M.L. Campeones", "M+ Liga de Campeones")
-            elif "#Vamos" in channel_name:
-                channel_name = channel_name.replace("#Vamos", "M+ #Vamos")
+            #elif "M.L. Campeones" in channel_name:
+                #channel_name = channel_name.replace("M.L. Campeones", "M+ Liga de Campeones")
+            #elif "#Vamos" in channel_name:
+                #channel_name = channel_name.replace("#Vamos", "M+ #Vamos")
                 
             channel_dict[channel_id] = channel_name
             
